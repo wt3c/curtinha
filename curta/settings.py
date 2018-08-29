4ab4db406f530ace23691796010e39fc580deed1
Welington Carlos <wcarlos3@gmail.com>
2018-08-29 14:22:30 -0300

    Problema com Heroku - Testes

--- curta/settings.py
+++ curta/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['curtinha.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://curtinha.herokuapp.com/','curtinha.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
